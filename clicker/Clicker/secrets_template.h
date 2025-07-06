4e6328fc2ef3ba783111b61478d496080dc25217
Andrei Niculescu <a.niculescu@student.fontys.nl>
2025-07-07 02:52:32 +0300

    small addition to secrets template

--- clicker/Clicker/secrets_template.h
+++ clicker/Clicker/secrets_template.h
@@ -1,5 +1,5 @@
 #define SECRET_SSID "" // initial WiFi network name
 #define SECRET_PASS "" // initial WiFi password
 #define SECRET_DPS_GLOBAL_ENDPOINT "global.azure-devices-provisioning.net"
-#define SECRET_DPS_ID_SCOPE "" // Azure DPS ID Scope
-#define SECRET_DPS_SYMMETRIC_KEY "" // base64 encoded key from Azure DPS enrollment group
+#define SECRET_DPS_ID_SCOPE ""      // Azure DPS ID Scope retrieved from the Azure Portal by going to the Azure DPS instance -> Overview
+#define SECRET_DPS_SYMMETRIC_KEY "" // base64 encoded key from Azure DPS enrollment group -> visible in the dashboard application under office management
